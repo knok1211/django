62c48e2151d7c43a182ede37b5e81f5472d22fa0
knok1211 <knok1211@gmail.com>
2025-10-25 20:21:01 +0900

    Update Django settings with new configurations

--- project1/settings.py
+++ project1/settings.py
@@ -75,13 +75,7 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = "svc.sel3.cloudtype.app", "31788"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -123,5 +117,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
