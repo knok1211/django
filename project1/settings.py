75fe1b429db6b91ad253a14bb63cbdd148d5070a
knok1211 <knok1211@gmail.com>
2025-10-25 20:08:48 +0900

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = ['https://*.cloudtype.app']
+
 
 # Application definition
 
@@ -121,4 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
